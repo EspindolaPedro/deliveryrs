3f41c394366436f74864a2bc3c47cdf4496cb08a
PedroPaulo <131112587+EspindolaPedro@users.noreply.github.com>
2025-03-15 19:01:30 -0400

    commit para pull da main

--- src/views/pages/product.php
+++ src/views/pages/product.php
@@ -13,73 +13,69 @@ $produtos = isset($_GET['produtos']) ? $_GET['produtos'] : '1';
     </div>
 
     <?php if ($produtos == '1'): ?>
-        
+
         <main class="container">
             <h1 class="text-3xl font-medium">Novo produto</h1>
 
-        <form id="productForm" class="form" method="POST" enctype="multipart/form-data" action="<?=$base?>/novo_produto" >
-            
-            <label class="labelAvatar" for="avatar">
-                <span>
-                    <img src="<?=$base?>/assets/images/upload.svg" alt="Ícone de Upload" width="30" height="30">
-                </span>
-                <input type="file" name="image_url" id="avatar" class="product" accept="image/png, image/jpeg" onchange="handleFile(event)">
-                <div class="preview-photo"></div>
-                
-            </label>
+            <form id="productForm" class="form" method="POST" enctype="multipart/form-data" action="<?= $base ?>/novo_produto">
 
-            <select name="category_id" id="category">
-                <option value="">Escolha uma categoria</option>
-                <?php foreach ($categories as $category):?>
-                    <option value="<?= htmlspecialchars($category['id'])?>"><?= htmlspecialchars($category['name'])?></option>
-                    <?php endforeach?>
-            </select>
-            
-            <!-- Campo de Nome do Produto -->
-            <input
-                type="text"
-                id="name"
-                placeholder="Digite o nome do produto"
-                class="input"
-                name="name"
-            />
+                <label class="labelAvatar" for="avatar">
+                    <span>
+                        <img src="<?= $base ?>/assets/images/upload.svg" alt="Ícone de Upload" width="30" height="30">
+                    </span>
+                    <input type="file" name="image_url" id="avatar" class="product" accept="image/png, image/jpeg" onchange="handleFile(event)">
+                    <div class="preview-photo"></div>
 
-            <!-- Campo de Preço do Produto -->
-            <div class="flex gap-2">
-                <input
-                    type="text"
-                    id="price"
-                    placeholder="Valor R$"
-                    class="input"
-                    name="price"
-                />
-                <input
-                    type="text"
-                    id="price_from"
-                    placeholder="Valor R$ (valor para promoção)"
-                    class="input"
-                    name="price_from"
-                />
-
-            </div>
-
-            <!-- Campo de Descrição do Produto -->
-            <textarea
-                id="description"
-                placeholder="Descreva seu produto"
-                class="input"
-                name="description"
-            ></textarea>
-
-            
-            <label for="visivel" class="text-xl" style="margin-bottom: 22px;">Aparecer no menu? <br>
-                <input type="checkbox" name="is_listed" value="1" checked id=""> Sim <br>
                 </label>
-            <!-- Botão de Cadastro -->
-            <button type="submit" class="buttonAdd">
-                Cadastrar
-            </button>
-        </form>
+
+                <select name="category_id" id="category">
+                    <option value="">Escolha uma categoria</option>
+                    <?php foreach ($categories as $category): ?>
+                        <option value="<?= htmlspecialchars($category['id']) ?>"><?= htmlspecialchars($category['name']) ?></option>
+                    <?php endforeach ?>
+                </select>
+
+                <!-- Campo de Nome do Produto -->
+                <input
+                    type="text"
+                    id="name"
+                    placeholder="Digite o nome do produto"
+                    class="input"
+                    name="name" />
+
+                <!-- Campo de Preço do Produto -->
+                <div class="flex gap-2">
+                    <input
+                        type="text"
+                        id="price"
+                        placeholder="Valor R$"
+                        class="input"
+                        name="price" />
+                    <input
+                        type="text"
+                        id="price_from"
+                        placeholder="Valor R$ (valor para promoção)"
+                        class="input"
+                        name="price_from" />
+
+                </div>
+
+                <!-- Campo de Descrição do Produto -->
+                <textarea
+                    id="description"
+                    placeholder="Descreva seu produto"
+                    class="input"
+                    name="description"></textarea>
+
+
+                <label for="visivel" class="text-xl" style="margin-bottom: 22px;">Aparecer no menu? <br>
+                    <input type="checkbox" name="is_listed" value="1" checked id=""> Sim <br>
+                </label>
+                <!-- Botão de Cadastro -->
+                <button type="submit" class="buttonAdd">
+                    Cadastrar
+                </button>
+            </form>
 
         </main>
 
